5ff2d254ba4a978a835cc7ec94eee0a2cfbd1f73
Vadim Soloviev <solovievadim@gmail.com>
2015-09-25 09:18:30 -0700

    submission
    data and a script

---
+++ run_analysis.r
@@ -0,0 +1,6 @@
+require(knitr)
+require(markdown)
+# setwd("~/GitHub repositories/GettingAndCleaningData/Project")
+setwd("~/Repositories/Coursera/GettingAndCleaningData/Project")
+knit("run_analysis.Rmd", encoding="ISO8859-1")
+markdownToHTML("run_analysis.md", "run_analysis.html")
